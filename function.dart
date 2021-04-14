58b048a6a968c3082e442ab5a467d0c123c04889
deecodez <obarodayo@gmail.com>
2021-04-15 00:43:30 +0100

    first commit

---
+++ function.dart
@@ -0,0 +1,26 @@
+void main() {
+  print(6 / 2 * (2 + 1));
+  // print(addNumber(10, 20));
+
+  // divideNumber(30, 2);
+
+  // print(multiply());
+
+  // myName('Obaro Oludayo Michael');
+}
+
+// int addNumber(int num1, int num2) {
+//   return num1 + num2;
+// }
+
+// void divideNumber(int num1, int num2) {
+//   print(num1 / num2);
+// }
+
+// int multiply({int num1: 1, int num2: 2, int num3: 3, int num4: 4}) {
+//   return num1 * num2 * num3 * num4;
+// }
+
+// void myName(String name) {
+//   print(name);
+// }
